37ff92bfb82571192f0734db805d7daf7729acb7
GianluViv <gianluca.vivenzi@gmail.com>
2026-04-04 16:03:36 +0200

    Progetto ripulito

--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/home/gian/.espressif/v5.4.3/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
--- build/esp-idf/mbedtls/mbedtls/library/version_features.c
+++
@@ -1,1 +0,0 @@
-/home/gian/.espressif/v5.4.3/esp-idf/components/mbedtls/mbedtls/library/version_features.c
